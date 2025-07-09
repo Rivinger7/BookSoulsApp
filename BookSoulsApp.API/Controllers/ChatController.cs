b51644ccadc9466fa8528b2c70d487881b19f1e1
Okasu <88636070+Rivinger7@users.noreply.github.com>
2025-07-09 14:19:35 +0700

    Update chat endpoints to use 'Customer' role and route param
    Changed authorization roles from 'User' to 'Customer' for chat endpoints. Modified the GetMessages endpoint to accept conversationId as a route parameter instead of a query parameter.

--- BookSoulsApp.API/Controllers/ChatController.cs
+++ BookSoulsApp.API/Controllers/ChatController.cs
@@ -11,15 +11,15 @@ public class ChatController(IChatService chatService) : ControllerBase
 {
     private readonly IChatService _chatService = chatService;
 
-    [Authorize(Roles = "Staff,User"), HttpGet("conversation")]
+    [Authorize(Roles = "Staff,Customer"), HttpGet("conversation")]
     public IActionResult CreateConversation()
     {
         var result = _chatService.GetConversationsAsync();
         return Ok(new { message = "Get conversations successfully.", result });
     }
 
-    [Authorize(Roles = "Staff,User"), HttpGet("messages")]
-    public IActionResult GetMessages([FromQuery] string conversationId)
+    [Authorize(Roles = "Staff,Customer"), HttpGet("messages/{conversationId}")]
+    public IActionResult GetMessages(string conversationId)
     {
         var result = _chatService.GetMessagesAsync(conversationId);
         return Ok(new { message = "Get messages successfully.", result });
